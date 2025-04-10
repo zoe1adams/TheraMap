acee12c28ad05e279f3799db7e1daf6d198284a5
zoe1adams <123134186+zoe1adams@users.noreply.github.com>
2025-04-10 14:59:49 -0400

    Delete theramap directory

--- theramap/theramap/backend/routes/therapists.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getTherapists } = require('../controllers/therapistController');
-
-router.get('/', getTherapists);
-
-module.exports = router;
-
