08f516c285ed17509632d90812de9457bd61fce9
zoe1adams <123134186+zoe1adams@users.noreply.github.com>
2025-04-10 14:10:54 -0400

    Update therapists.js

--- theramap/theramap/backend/routes/therapists.js
+++ theramap/theramap/backend/routes/therapists.js
@@ -1,1 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const { getTherapists } = require('../controllers/therapistController');
+
+router.get('/', getTherapists);
+
+module.exports = router;
 
